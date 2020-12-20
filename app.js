8e685014d48780e70fe5043dda666c0829526873
dswaraj105 <57761161+dswaraj105@users.noreply.github.com>
2020-12-20 21:58:07 +0545

    tweeked required frontend
    Adding the modified html files, related images, css file and some additional base demo  setups for node

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('hi');
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/product', (req, res, next) => {
+  res.send('Wanna buy Something');
+});
+ 
+app.use('/', (req, res, next) => {
+  res.send('welcome to my shop');
+});
+
+
+
+app.listen(3000);
